26f2a8743650fea6f861fe343ad1888aa8bb2a07
ramonreyes-create <ramon.reyes@csuv.cl>
2026-06-30 21:40:59 -0400

    sprint2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,4 @@
 
-console.log("DeutschQuest 4.0 Sprint 1.1A");
+const sb=document.getElementById('sidebar');
+document.getElementById('toggle').onclick=()=>sb.classList.toggle('collapsed');
+document.getElementById('themeBtn').onclick=()=>document.body.classList.toggle('dark');
